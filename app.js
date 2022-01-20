6d904508e5123d2c10985fe894b283741d4ea7a5
saretoduncan <saretoduncan@gmail.com>
2022-01-20 12:32:20 +0300

    add routes *user, *events

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import userAuth_routes from "./src/routes/userRoutes.js";
+import events_routes from "./src/routes/eventsRoutes.js";
 
 const app = express();
 //config
 dotenv.config();
 //port
 const port = process.env.PORT || 5000;
+
 //db_connection
-mongoose.connect(process.env.MONGODB_URI,()=>{
-    console.log('db connected successfully')
-})
+mongoose.connect(process.env.MONGODB_URI, () => {
+  console.log("db connected successfully");
+});
+//middlewares
+app.use(express.json());
+app.use("/api/user", userAuth_routes);
+app.use("/api/event", events_routes);
 //server
 app.listen(port, () =>
   console.log(`server is running on port http://localhost:${port}`),
